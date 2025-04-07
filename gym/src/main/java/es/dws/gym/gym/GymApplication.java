bf29014cd2080b79ec45ab840102f8a9b2d106c4
EnriqueEscobar-URJC <e.escobar.2022@alumnos.urjc.es>
2025-04-08 01:52:18 +0200

    EnableSpringDataWebSupport

--- gym/src/main/java/es/dws/gym/gym/GymApplication.java
+++ gym/src/main/java/es/dws/gym/gym/GymApplication.java
@@ -2,11 +2,12 @@ package es.dws.gym.gym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 // Main class for starting the Gym application using Spring Boot.
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class GymApplication {
-
 
 	// The entry point of the Gym application.
 	public static void main(String[] args) {
