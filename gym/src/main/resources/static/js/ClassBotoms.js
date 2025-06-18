86c82a57c3515be39d32837d221a22c4e00083f7
EnriqueEscobar-URJC <e.escobar.2022@alumnos.urjc.es>
2025-06-18 11:39:54 +0200

    ClassBotoms update post

--- gym/src/main/resources/static/js/ClassBotoms.js
+++ gym/src/main/resources/static/js/ClassBotoms.js
@@ -3,7 +3,18 @@ function edit(id){
 }
 
 function remove(id){
-    window.location.href = "/gymclass/"+ id + "/delete";
+    var form = document.createElement("form");
+    form.method = "POST";
+    form.action = "/gymclass/" + id + "/delete";
+
+    var input = document.createElement("input");
+    input.type = "hidden";
+    input.name = "action";
+    input.value = "true";
+    form.appendChild(input);
+
+    document.body.appendChild(form);
+    form.submit();
 }
 
 function register(id){
